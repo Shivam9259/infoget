c71eb8415b8db136d0bb220b185410e74e145b1e
Shivam9259 <47673274+Shivam9259@users.noreply.github.com>
2020-03-28 23:28:22 +0530

    Other Files Uploaded
    ...

---
+++ layout/AdminLayout/Footer.php
@@ -0,0 +1,68 @@
+    </div>
+    </main>
+
+    <!--Footer-->
+    <footer class="page-footer text-center font-small mdb-color darken-2 mt-4 wow fadeIn">
+
+      <hr class="my-4">
+
+      <!-- Social icons -->
+      <div class="pb-4">
+        <a href="#">
+          <i class="fab fa-facebook-f mr-3"></i>
+        </a>
+
+        <a href="#">
+          <i class="fab fa-twitter mr-3"></i>
+        </a>
+
+        <a href="#">
+          <i class="fab fa-youtube mr-3"></i>
+        </a>
+
+        <a href="#">
+          <i class="fab fa-google-plus-g mr-3"></i>
+        </a>
+
+        <a href="#">
+          <i class="fab fa-dribbble mr-3"></i>
+        </a>
+
+        <a href="#">
+          <i class="fab fa-pinterest mr-3"></i>
+        </a>
+
+        <a href="#">
+          <i class="fab fa-github mr-3"></i>
+        </a>
+
+        <a href="#">
+          <i class="fab fa-codepen mr-3"></i>
+        </a>
+      </div>
+      <!-- Social icons -->
+
+      <!--Copyright-->
+      <div class="footer-copyright py-3">
+        © 2019 Copyright
+      </div>
+      <!--/.Copyright-->
+
+    </footer>
+    <!--/.Footer-->
+
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="resources/js/jquery-3.4.1.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="resources/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="resources/js/mdb.min.js"></script>
+    <!-- Initializations -->
+    <script type="text/javascript">
+      // Animations initialization
+      new WOW().init();
+    </script>
+    </body>
+
+    </html>
---
+++ layout/HomeLayout/Header.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+if (isset($_SESSION['loggedin'])) {
+  header('Location:dashboard.php');
+}
+?>
+<?php include('dbconfig.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Infoget App</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="resources/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="resources/css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="resources/css/style.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <header>
+    <!-- Navbar -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar">
+      <div class="container">
+        <!-- Brand -->
+        <a class="navbar-brand waves-effect" href="#">
+          <strong class="blue-text">Infoget App</strong>
+        </a>
+
+        <!-- Collapse -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Links -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+          <!-- Left -->
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link waves-effect" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link waves-effect" href="blogs.php">Blogs</a>
+            </li>
+          </ul>
+
+          <!-- Right -->
+          <ul class="navbar-nav nav-flex-icons">
+
+            <li class="nav-item">
+              <a href="admin.php" class="nav-link border border-light rounded waves-effect" target="_blank"><i class="fas fa-sign-in-alt"></i>Admin Login</a>
+            </li>
+          </ul>
+
+        </div>
+
+      </div>
+    </nav>
+    <!-- Navbar -->
+
+  </header>
+  <main class="mt-5 pt-5">
+    <div class="container">
